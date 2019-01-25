e1092793a0dc1c27469d8ddf0f2d749856c9dfc3
prio <miprio101@gmail.com>
2019-01-26 02:26:51 +0600

    solved task 2 build api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,22 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def movies_list
+    @movies = Movie.select(:id, :title)
+    render json: { data: @movies }, status: :ok
+  end
+
+  def movie
+    @movie = Movie.find(params[:id])
+    render json: { data: @movie }, status: :ok
+  end
+
+  def movies_list_with_genre
+    @movies = Movie.all
+    json = MovieSerializer.new(@movies).serialize
+    render json: { data: json }, status: :ok
+  end
+
   def send_info
     @movie = Movie.find(params[:id])
     MovieInfoMailer.send_info(current_user, @movie).deliver_now
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,15 @@ Rails.application.routes.draw do
       get :export
     end
   end
+
+  scope :api do
+    scope :v1 do
+      get "movies_list", to: "movies#movies_list", as: :movies_list
+      get "movie/:id", to: "movies#movie", as: :movie_data
+    end
+
+    scope :v2 do
+      get "movie_list", to: "movies#movies_list_with_genre", as: :movies_list_with_genre
+    end
+  end
 end
