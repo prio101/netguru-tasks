9c00a1902e65694ffc9290e971720e9ee450233c
prio <miprio101@gmail.com>
2019-01-27 23:54:45 +0600

    Added the spec change to avoid ghost fail

--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 describe "Genres requests", type: :request do
-  let!(:genres) { create_list(:genre, 5, :with_movies) }
-
+  let!(:genre) { FactoryBot.create(:genre) }
+  let!(:movie) { FactoryBot.create(:movie, genre_id: genre.id)}
   describe "genre list" do
     it "displays only related movies" do
-      visit "/genres/" + genres.sample.id.to_s + "/movies"
-      expect(page).to have_selector("table tr", count: 5)
+      visit "/genres/" + genre.id.to_s + "/movies"
+      expect(page).to have_selector("table tr", count: 1)
     end
   end
 end
